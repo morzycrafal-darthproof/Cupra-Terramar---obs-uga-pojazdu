09103129248e2e2297b6487dcc8a6363f4a50241
Rafał <gd11838@student.gdansk.merito.pl>
2026-08-09 08:08:21 +0200

    Add files via upload
    Nowa ikona

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cupra-terramar-v2";
+const CACHE = "cupra-terramar-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
